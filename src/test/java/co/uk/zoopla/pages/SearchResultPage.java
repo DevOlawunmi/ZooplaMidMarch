19f8ed0b9e5a3dddc64743d2dcf253210fd1f49b
Ola <olawunmi263@yahoo.com>
2020-03-17 04:15:28 +0000

    Search Result Page Displayed

--- src/test/java/co/uk/zoopla/pages/SearchResultPage.java
+++ src/test/java/co/uk/zoopla/pages/SearchResultPage.java
@@ -1,7 +1,13 @@
 package co.uk.zoopla.pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+import java.util.Random;
 
 public class SearchResultPage extends BasePage
 {
@@ -10,4 +16,34 @@ public class SearchResultPage extends BasePage
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+    @FindBy (css = "#content h1")
+    private WebElement pageTitle;
+    @FindBy (css = "listing-results-price.text-price")
+    public List<WebElement>results;
+
+    public void isSearchResultDisplayed()
+    {
+        Assert.assertTrue(pageTitle.isDisplayed());
+    }
+    public void isCorrectURLDisplayed(String location)
+    {
+        driver.getCurrentUrl().contains(location.toLowerCase());
+    }
+    public void isPropertyTypeDisplayed(String property)
+    {
+       String title= pageTitle.getText();
+       Assert.assertTrue(title.contains(property));
+    }
+    public void isLocationDisplayed(String location)
+    {
+String title = pageTitle.getText();
+Assert.assertTrue(title.contains(location));
+    }
+public ProductDetailsPage clickOnARandomResult()
+{
+    Random random = new Random();
+    int randomNumber = random.nextInt(results.size()-1);
+    results.get(randomNumber).click();
+        return new ProductDetailsPage(driver);
+}
 }
--- src/test/java/co/uk/zoopla/stepDefinitions/SearchForSaleSteps.java
+++ src/test/java/co/uk/zoopla/stepDefinitions/SearchForSaleSteps.java
@@ -2,6 +2,7 @@ package co.uk.zoopla.stepDefinitions;
 
 import co.uk.zoopla.pages.BasePage;
 import co.uk.zoopla.pages.Homepage;
+import co.uk.zoopla.pages.ProductDetailsPage;
 import co.uk.zoopla.pages.SearchResultPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -12,6 +13,7 @@ public class SearchForSaleSteps extends BasePage
 {
     Homepage homepage = PageFactory.initElements(driver, Homepage.class);
     SearchResultPage searchResultPage = PageFactory.initElements(driver, SearchResultPage.class);
+    ProductDetailsPage productDetailsPage = PageFactory.initElements(driver, ProductDetailsPage.class);
 
     @Given("I navigate to zoopla homepage")
 public void i_navigate_to_zoopla_homepage()
@@ -20,43 +22,52 @@ launchURL();
 }
 
     @When("I enter a {string} in the search text box")
-    public void i_enter_a_in_the_search_text_box(String string) {
-
+    public void i_enter_a_in_the_search_text_box(String location) {
+homepage.enterLocation(location);
     }
 
     @When("I select {string} from Min price dropdown")
-    public void i_select_from_Min_price_dropdown(String string) {
-
+    public void i_select_from_Min_price_dropdown(String miniPrice)
+    {
+homepage.selectMinimumPrice(miniPrice);
     }
 
     @When("I select {string} from Max price dropdown")
-    public void i_select_from_Max_price_dropdown(String string) {
-
+    public void i_select_from_Max_price_dropdown(String maxiPrice)
+    {
+homepage.selectMaximumPrice(maxiPrice);
     }
 
     @When("I select {string} from Property type dropdown")
-    public void i_select_from_Property_type_dropdown(String string) {
-
+    public void i_select_from_Property_type_dropdown(String property) {
+homepage.selectPropertyType(property);
     }
 
     @When("I select {string} from Bedrooms dropdown")
-    public void i_select_from_Bedrooms_dropdown(String string) {
-
+    public void i_select_from_Bedrooms_dropdown(String beds)
+    {
+homepage.selectNoOfBeds(beds);
     }
 
     @When("I click on Search button")
-    public void i_click_on_Search_button() {
-
+    public void i_click_on_Search_button()
+    {
+searchResultPage = homepage.clickOnSearchButton();
     }
 
     @Then("a list of {string} in {string} are displayed")
-    public void a_list_of_in_are_displayed(String string, String string2) {
+    public void a_list_of_in_are_displayed(String property, String location)
+    {searchResultPage.isSearchResultDisplayed();
+        searchResultPage.isCorrectURLDisplayed(location);
+        searchResultPage.isPropertyTypeDisplayed(property);
+    searchResultPage.isLocationDisplayed(location);
 
     }
 
     @Then("I click on one of the results links")
-    public void i_click_on_one_of_the_results_links() {
-
+    public void i_click_on_one_of_the_results_links()
+    {
+productDetailsPage = searchResultPage.clickOnARandomResult();
     }
 
 
